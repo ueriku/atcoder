71fd8becd69d446558a0995e46a852ddc6ae285e
ueriku <ue.riku@gmail.com>
2017-09-14 23:27:38 +0900

    ABC #072のプログラムを追加

---
+++ atcoder-prg/src/abc072/c/Main.java
@@ -0,0 +1,172 @@
+package abc072.c;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+
+public class Main {
+    FastScanner in = new FastScanner(System.in);
+    PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        new Main().calc();
+    }
+    
+    public void calc() {
+        int N = in.nextInt();
+        int a[] = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            a[i] = in.nextInt();
+        }
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        for (int i = 0; i < N; i++) {
+            map.put(a[i], map.getOrDefault(a[i], 0) + 1);
+            map.put(a[i] + 1, map.getOrDefault(a[i] + 1, 0) + 1);
+            map.put(a[i] - 1, map.getOrDefault(a[i] - 1, 0) + 1);
+        }
+
+        int ans = 0;
+        
+        for (Map.Entry<Integer, Integer> e: map.entrySet()) {
+            // out.println(e.getKey() + ", " + e.getValue());
+            ans = Math.max(e.getValue(), ans);
+        }
+        
+        out.println(ans);
+        out.close();
+    }
+    
+    class FastScanner {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public FastScanner(InputStream stream) {
+            this.stream = stream;
+        }
+
+        int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        boolean isEndline(int c) {
+            return c == '\n' || c == '\r' || c == -1;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] nextIntArray(int n) {
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+                array[i] = nextInt();
+
+            return array;
+        }
+
+        int[][] nextIntMap(int n, int m) {
+            int[][] map = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                map[i] = in.nextIntArray(m);
+            }
+            return map;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        long[] nextLongArray(int n) {
+            long[] array = new long[n];
+            for (int i = 0; i < n; i++)
+                array[i] = nextLong();
+
+            return array;
+        }
+
+        long[][] nextLongMap(int n, int m) {
+            long[][] map = new long[n][m];
+            for (int i = 0; i < n; i++) {
+                map[i] = in.nextLongArray(m);
+            }
+            return map;
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        double[] nextDoubleArray(int n) {
+            double[] array = new double[n];
+            for (int i = 0; i < n; i++)
+                array[i] = nextDouble();
+
+            return array;
+        }
+
+        double[][] nextDoubleMap(int n, int m) {
+            double[][] map = new double[n][m];
+            for (int i = 0; i < n; i++) {
+                map[i] = in.nextDoubleArray(m);
+            }
+            return map;
+        }
+
+        String next() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        String[] nextStringArray(int n) {
+            String[] array = new String[n];
+            for (int i = 0; i < n; i++)
+                array[i] = next();
+
+            return array;
+        }
+
+        String nextLine() {
+            int c = read();
+            while (isEndline(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isEndline(c));
+            return res.toString();
+        }
+    }
+}
